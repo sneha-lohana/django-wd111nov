4f908b3cefd1d583a5826f2f3c949f0272b94169
Sneha Lohana <sneha.lohana16@gmail.com>
2019-12-25 16:00:51 +0530

    Added carts app and Cart Model

---
+++ onlinesale/carts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+class Cart(models.Model):
+    user        = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    products    = models.ManyToManyField(Product, blank=True)
+    subtotal    = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    total       = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    updated     = models.DateTimeField(auto_now=True)
+    created   = models.DateTimeField(auto_now_add=True)
--- onlinesale/onlinesale/settings.py
+++ onlinesale/onlinesale/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts', 'products',
+    'accounts', 'products', 'carts', 
 ]
 
 MIDDLEWARE = [
