43699a08bbf306af83e48ab0888af3e8265f46e0
Sneha Lohana <sneha.lohana16@gmail.com>
2020-01-08 12:38:31 +0530

    Admin Site modifications

--- onlinesale/accounts/admin.py
+++ onlinesale/accounts/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+class UserAdmin(BaseUserAdmin):
+    #modification in admin site for update form
+    form = UserAdminChangeForm
+    fieldsets = (
+        ('Personal Info', {'fields': ('full_name', 'mobile', 'email', 'password')}),
+        ('Permissions', {'fields': ('admin', 'staff', 'active',)}),
+    )
+    #modification in admin site add form (any model class)
+    add_form = UserAdminCreationForm
+    add_fieldsets = (
+        ("User Details", {
+            'classes': ('wide',),
+            'fields': ('full_name', 'mobile','email')}
+        ),
+        ("Password Details",
+        {
+            'classes' : ('wide',),
+            'fields' : ('password1', 'password2')
+        })
+    )
+
+    #modifications in view page of admin site
+    list_filter = ('active','admin','staff')
+    filter_horizontal = ()
+    ordering = ('full_name',)
+    list_display = ('full_name','email')
+    search_fields = ('full_name', 'email', 'mobile')
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
